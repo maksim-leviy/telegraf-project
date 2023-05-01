72e0f012b8e308e30554b66c7465b761ba8c99ea
Maksim Leviy <104758136+maksim-leviy@users.noreply.github.com>
2023-05-01 19:35:45 +0300

    Update language.js

--- src/handlers/actions/language.js
+++ src/handlers/actions/language.js
@@ -8,7 +8,7 @@ composer.action(/language:([^]+)/, async (ctx) => {
     let language = ctx.match[1]
     if (language === ctx.session.locale) return ctx.answerCbQuery(ctx.i18next.t('language.error'))
 
-    ctx.i18n.changeLanguage(language)
+    ctx.i18next.changeLanguage(language)
 
     return ctx.editMessageText(ctx.i18next.t('language.menu'), Klanguage(ctx))
 })
