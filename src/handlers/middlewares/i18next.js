c9490fd925d16f345cd20ed386bb9230d393aadc
Maksim Leviy <104758136+maksim-leviy@users.noreply.github.com>
2023-05-15 19:25:10 +0300

    Update i18next.js

--- src/handlers/middlewares/i18next.js
+++ src/handlers/middlewares/i18next.js
@@ -1,7 +1,7 @@
 const { Composer } = require('telegraf')
 const { i18next } = require('telegraf-i18next')
 
-const { gb, ru, ua } = require('../../locales')
+const locales = require('../../locales')
 
 const composer = new Composer()
 
@@ -9,7 +9,7 @@ composer.use(i18next({
     lng: 'ru',
     fallbackLng: 'ru',
     debug: true,
-    resources: { gb, ru, ua }
+    resources: locales
 }))
 
 module.exports = composer
